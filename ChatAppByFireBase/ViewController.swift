173606d13c0b9cde168761d5c2a6d0d0fbd5ae3f
Kevin-Xiaoran <kairanzhai@kairans-MacBook-Pro.local>
2017-09-28 21:14:59 -0400

    Initial Commit

---
+++ ChatAppByFireBase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatAppByFireBase
+//
+//  Created by Mr.小然 on 2017/9/28.
+//  Copyright © 2017年 kairan zhai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
